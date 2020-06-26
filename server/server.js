f778e6f4bb2fe1322c5bdaf394518e23207d60ee
SorinGabriel02 <lupu_sg@yahoo.com>
2020-06-26 21:53:52 +0300

    basic server setup

---
+++ server/server.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+
+const userRouter = require("./routes/usersRoutes");
+
+const app = express();
+
+// middleware
+app.use(morgan("dev"));
+app.use(express.json());
+
+// routes
+app.use("/api/users", userRouter);
+
+const port = process.env.PORT || 8080;
+
+mongoose.connect(
+  process.env.MONGO_URI,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err) => {
+    if (err) console.log(err);
+    console.log("Successfully connected to the database.");
+    app.listen(port, () => console.log(`Server listening on port ${port}`));
+  }
+);
